6fbd5ebe772bb720ef5605ad2696cde491c56e2f
piartemov <api24@mail.ru>
2023-11-10 13:12:55 +0300

    екое

--- ProgDjango/main/models.py
+++ ProgDjango/main/models.py
@@ -12,10 +12,44 @@ class Articales(models.Model):
         new_filename = f'{self.title}.{ext}'
         full_path = os.path.join(media_path, new_filename)
         return full_path
+
+    def extension(self):
+        name, extension = os.path.splitext(self.picture.name)
+        return extension
+
+    def image_extensions(self):
+        return ['.jpg', '.png', '.jpeg', '.webp']
+
     title = models.CharField('название', max_length=50)
     anons = models.CharField('анонс', max_length=250)
     text = models.TextField('статья')
     time = models.DateTimeField('время публикации')
     picture = models.FileField(upload_to=file_path.__func__, null=True)
+    def str(self):
+        return self.title
+
+
+class Custom_user(models.Model):
+    @staticmethod
+    def file_path(self, filename):
+        media_path = settings.MEDIA_ROOT + 'articles/' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        ext = filename.split('.')[-1]
+        new_filename = f'{self.title}.{ext}'
+        full_path = os.path.join(media_path, new_filename)
+        return full_path
+
+    def extension(self):
+        name, extension = os.path.splitext(self.picture.name)
+        return extension
+
+    def image_extensions(self):
+        return ['.jpg', '.png', '.jpeg', '.webp']
+
+    title = models.CharField('название', max_length=50)
+    anons = models.CharField('анонс', max_length=250)
+    text = models.TextField('статья')
+    time = models.DateTimeField('время публикации')
+    picture = models.FileField(upload_to=file_path.__func__, null=True)
+    avatar = models.FileField()
     def str(self):
         return self.title
